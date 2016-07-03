7cfb023389678e5995a261b499c8aa6901db031f
benevolent0505 <mikio.fjt@gmail.com>
2016-07-04 05:33:33 +0900

    [WIP] Create view and controller

---
+++ php/app/controllers/IndexController.php
@@ -0,0 +1,9 @@
+<?php
+namespace SlideViewShare;
+
+class IndexController {
+
+  public function index() {
+
+  }
+}
--- php/slideviewshare.php
+++ php/slideviewshare.php
@@ -1,3 +1,31 @@
 <?php
+namespace SlideViewShare;
 
-echo "Home";
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/app/libs/request.php';
+require_once __DIR__ . '/app/libs/router.php';
+
+// Requestを受けとり
+dumpArray($_SERVER);
+
+$request = new Request($_SERVER);
+
+$routing_map = array(
+  array('GET', '/', ['IndexController', 'index']),
+);
+
+$loader = new \Twig_Loader_Filesystem(__DIR__ . '/app/views');
+$twig = new \Twig_Environment($loader);
+
+echo $twig->render('index.tpl.html');
+
+
+
+function dumpArray(array $arr) {
+  foreach($arr as $key => $value) {
+      var_dump($key);
+      echo(" : ");
+      var_dump($value);
+      echo("<br>");
+  }
+}
