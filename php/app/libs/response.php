3ae6374cbd4c7e555d8eab68f39ffc9b681c7ced
benevolent0505 <mikio.fjt@gmail.com>
2016-07-04 05:32:44 +0900

    [WIP] create Response, Router

---
+++ php/app/libs/response.php
@@ -0,0 +1,12 @@
+<?php
+namespace SlideViewShare;
+
+class Response {
+  public $status;
+  public $header;
+  public $body;
+
+  public function __construct() {
+
+  }
+}
---
+++ php/app/libs/router.php
@@ -0,0 +1,11 @@
+<?php
+namespace SlideViewShare;
+
+class Router {
+
+  public function __construct(array $routing_map) {
+  }
+
+  public function matche($request) {
+  }
+}
