f8d229022fd9ecde066091195292b54ad6cbd0ea
benevolent0505 <mikio.fjt@gmail.com>
2016-07-12 21:55:35 +0900

    IndexページとSigninページを表示出来るようにした
    感想: https://twitter.com/benevolent0505/status/752737555339022336

--- php/app/controllers/IndexController.php
+++ php/app/controllers/IndexController.php
@@ -1,9 +1,22 @@
 <?php
-namespace SlideViewShare;
+namespace SlideViewShare\controllers;
+
+require_once __DIR__ . "/../helpers/functions.php";
 
 class IndexController {
 
-  public function index() {
+  public function __construct(){
+  }
 
+  public function show() {
+    return function () {
+      return renderResponse('index.tpl.html');
+    };
+  }
+
+  public function signup() {
+    return function () {
+      return renderResponse('signup.tpl.html');
+    };
   }
 }
--- php/slideviewshare.php
+++ php/slideviewshare.php
@@ -1,31 +1,24 @@
 <?php
 namespace SlideViewShare;
 
-require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/app/libs/request.php';
 require_once __DIR__ . '/app/libs/router.php';
 
-// Requestを受けとり
-dumpArray($_SERVER);
-
-$request = new Request($_SERVER);
-
-$routing_map = array(
-  array('GET', '/', ['IndexController', 'index']),
+$routing_map = array();
+$routing_map[] = array(
+  'GET', '/', array('IndexController', 'show'),
+);
+$routing_map[] = array(
+  'GET', '/signup', array('IndexController', 'signup')
 );
 
-$loader = new \Twig_Loader_Filesystem(__DIR__ . '/app/views');
-$twig = new \Twig_Environment($loader);
+$router = new Router($routing_map);
+$request = new Request($_SERVER);
+$response = $router->match($request);
 
-echo $twig->render('index.tpl.html');
+$controller = $response->controller;
+$method = $response->controller_method;
 
+$content = call_user_func($controller->$method());
 
-
-function dumpArray(array $arr) {
-  foreach($arr as $key => $value) {
-      var_dump($key);
-      echo(" : ");
-      var_dump($value);
-      echo("<br>");
-  }
-}
+echo $content;
