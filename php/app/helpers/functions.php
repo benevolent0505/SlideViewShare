fdebaf3d5922c400aa3938e4b16a36d4b6a636d5
benevolent0505 <mikio.fjt@gmail.com>
2016-07-25 10:40:58 +0900

    テンプレートを分割し、パスを正しく取得できるようにした

--- php/app/helpers/functions.php
+++ php/app/helpers/functions.php
@@ -5,6 +5,7 @@ require_once __DIR__ . "/../../vendor/autoload.php";
 function renderResponse($filename, array $params=array()) {
   $loader = new \Twig_Loader_Filesystem(__DIR__ . "/../views");
   $twig = new \Twig_Environment($loader);
+  $twig->addGlobal('server', $_SERVER);
 
   return $twig->render($filename, $params);
 }
